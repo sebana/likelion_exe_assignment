ac7a05f5111ac51966e9181c1dbff882b5b3e306
sebana <38051464+sebana@users.noreply.github.com>
2019-03-04 03:12:55 +0900

    pagination

--- model/second/blog/views.py
+++ model/second/blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 def home(request):
     blogs = Blog.objects
-    return render(request, 'home.html', {'blogs':blogs})
+    #모든 블로그 글들을 대상으로 한다.
+    blog_list = Blog.objects.all()
+    #블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    #request된 페이지가 뭔지를 알아내고
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return해주기
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs':blogs, 'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
